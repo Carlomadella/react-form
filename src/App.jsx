1a240014f69f0b604fac7cabe3e699c7fdc2d5d0
Carlomadella <madella871@gmail.com>
2025-05-09 18:55:45 +0200

    [UPD] creato array per completare MIlestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@
 const players = ['Ter Stegen', 'Thiago Silva', 'Messi', 'Tonali', 'Lewandowski'] 
 
 function App() {
+  
   return (
     <>
-      
+      <div className="container">
+        <h1>5-a-side</h1>
+        <ul className="list-group mb-4">
+          {players.map((player,index) =>(
+            <li key={index} className="list-group-item">{player}</li>
+          ))}
+        </ul>
+        
+      </div>
     </>
   )
 }
