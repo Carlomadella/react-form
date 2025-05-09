5e043430aaed512c6e5cab5474674b1f1f3650db
Carlomadella <madella871@gmail.com>
2025-05-09 18:34:02 +0200

    [FEAT] creati file necessari

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+const players = ['Ter Stegen', 'Thiago Silva', 'Messi', 'Tonali', 'Lewandowski'] 
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
